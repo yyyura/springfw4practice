702ba00077f60ea5db245341a79dfe0478398b1e
Jurijs Cicelimovs <yyyura@gmail.com>
2021-04-13 11:20:23 +0100

    lesson 6a - DI using Setter and ref="musicBean"

--- src/main/java/ie/yyyura/springfw/Player.java
+++ src/main/java/ie/yyyura/springfw/Player.java
@@ -4,12 +4,23 @@ public class Player {
 
     private MusicInterface mi;
 
-    //IoC - Constructor
+    // lesson 5 - IoC - dependency injection using Constructor
     public Player(MusicInterface mi) {
         this.mi = mi;
+    }
+
+    // pustoj konstruktor, esli be mi ne sozdali svoj - to Java bi sozdala ego za nas
+    public Player() {
+    }
+
+    // lesson 6 - DI using Setter
+    // Spring udaljaet "set" "MusicSetter" delaet s malenkoj i v .xml ispolzuem "musicSetter"->
+    public void setMusicSetter(MusicInterface miIN) {
+        this.mi = miIN;
     }
 
     public void play() {
         System.out.println("Playing " + mi.getSong());
     }
 }
+   
